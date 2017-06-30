795a55757ace0963c8d030cd8524bd278bb8947f
Phaulo71 <escaphau@my.dom.edu>
2017-06-30 14:32:23 -0500

    Started the setup file for Pypi.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+from setuptools import setup
+
+setup(
+  name = 'xPlotUtil',
+  packages = ['xPlotUtil'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'The program provides a GUI for the user to graph the data in different forms, normalize and fit it.',
+  author = 'Phaulo C. Escalante',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/AdvancedPhotonSource/xPlotUtil', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
