748542e995c69680401a533c7a105db8c5826cac
Phaulo71 <PhEs624>
2017-07-05 16:24:36 -0500

    Adding xPlotUtil to pypi repository

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ See LICENSE file.
 from setuptools import setup
 
 setup(
-  name = 'xPlotUtil',
-  packages = ['xPlotUtil'], # this must be the same as the name above
-  version = '0.0.1',
-  description = 'The program provides a GUI for the user to graph the data in different forms, normalize and fit it.',
-  author = 'Phaulo C. Escalante',
-  author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/AdvancedPhotonSource/xPlotUtil', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  name='xPlotUtil',
+  version='0.0.6',
+  description='The program provides a GUI for the user to graph the data in different forms, normalize and fit it.',
+  author='Phaulo C. Escalante',
+  author_email='escalante.phaulo@outlook.com',
+  url='https://github.com/AdvancedPhotonSource/xPlotUtil',
+  packages=['xPlotUtil', ],
+  install_requires=['spec2nexus',
+                    'matplotlib',
+                    'numpy',
+                    'future',
+                    'scipy',
+                    'peakutils',
+                    ],
+  license='See LICENSE File',
+  platforms='any',
+  scripts=['Scripts/xPlotUtil.cmd', ]
 )
